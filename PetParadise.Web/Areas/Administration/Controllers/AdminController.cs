fd5b0b991443c7dda3a5a31c7834aaa962e21507
TodorVyagov <rambo007@abv.bg>
2014-11-17 22:02:45 +0200

    Added Administration area.

---
+++ PetParadise.Web/Areas/Administration/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿namespace PetParadise.Web.Areas.Administration.Controllers
+{
+    using System.Web.Mvc;
+
+    using PetParadise.Common;
+    using PetParadise.Data;
+    using PetParadise.Web.Controllers;
+
+    [Authorize(Roles=GlobalConstants.AdminRole)]
+    public class AdminController : BaseController
+    {
+        public AdminController(IPetParadiseData data)
+            : base(data)
+        {
+        }
+    }
+}
